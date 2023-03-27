03d25ffb2051d51ca4eac7d0358c8e6bd28b8b57
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-27 16:16:43 +0200

    nuovo import Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './assets/styles/main.scss'  // agganciato file main.scss come css principale
+import * as bootstrap from 'bootstrap' // import bootstrap per JavaScript (da documentazione Bootstrap/Vite)
 
 import App from './App.vue'
 
