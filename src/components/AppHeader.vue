bf026828fd767d72ffbf26110d26a4740af57bea
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-28 00:20:34 +0200

    sistemazione header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,7 +9,8 @@ export default {
 <!-- PARTE HTML -->
 <template>
     <header>
-        <div class="container-fluid">
+        <div class="container-fluid d-flex align-items-center">
+            <div class="nb_logo_box mx-3"><img class=img-fluid src="../../public/images/yugioh.webp" alt="logo"></div>
             <h1>Yu-Gi-Oh Api</h1>
         </div>
     </header>
@@ -20,5 +21,11 @@ export default {
 header {
     padding: 1.25rem 0;
     background-color: white;
+
+    .nb_logo_box {
+        width: 3.125rem;
+        height: 1.875rem;
+
+    }
 }
 </style>
